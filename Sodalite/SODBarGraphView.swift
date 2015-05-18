235e90abbb97ad01563766f298a4ba5c19087be1
S-Shimotori <S-Shimotori@S-Shimotoris-MacBook-Pro.local>
2015-05-18 10:53:33 +0900

    clean bar class
    make barsClass private

--- Sodalite/SODBarGraphView.swift
+++ Sodalite/SODBarGraphView.swift
@@ -24,48 +24,46 @@ class SODBarGraphView: SODGraphViewWithAxis {
         graphView.addSubview(bars)
     }
 
-}
-
-
-class Bars: UIView {
-    var data = [Double]()
-    var barWidth: CGFloat = 0
-    var globalMaxValueInGraph: Double = 0
-    
-    init(frame: CGRect, data: [Double], barWidth: CGFloat?, globalMaxValueInGraph: Double?) {
-        self.data = data
+    class Bars: UIView {
+        var data = [Double]()
+        var barWidth: CGFloat = 0
+        var globalMaxValueInGraph: Double = 0
         
-        if let barWidth = barWidth where barWidth > 0 {
-            self.barWidth = barWidth
-        } else {
-            self.barWidth = frame.width / CGFloat(data.count * 2)
+        init(frame: CGRect, data: [Double], barWidth: CGFloat?, globalMaxValueInGraph: Double?) {
+            self.data = data
+            
+            if let barWidth = barWidth where barWidth > 0 {
+                self.barWidth = barWidth
+            } else {
+                self.barWidth = frame.width / CGFloat(data.count * 2)
+            }
+            
+            if let globalMaxValueInGraph = globalMaxValueInGraph {
+                self.globalMaxValueInGraph = globalMaxValueInGraph > 0 ? globalMaxValueInGraph : 0
+            } else {
+                let maxValueOfData = maxElement(data)
+                self.globalMaxValueInGraph = maxValueOfData > 0 ? maxValueOfData : 0
+            }
+            
+            super.init(frame: frame)
         }
         
-        if let globalMaxValueInGraph = globalMaxValueInGraph {
-            self.globalMaxValueInGraph = globalMaxValueInGraph > 0 ? globalMaxValueInGraph : 0
-        } else {
-            let maxValueOfData = maxElement(data)
-            self.globalMaxValueInGraph = maxValueOfData > 0 ? maxValueOfData : 0
+        required init(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
         }
         
-        super.init(frame: frame)
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-
-    override func drawRect(rect: CGRect) {
-        for i in 0..<data.count {
-            let rectangle = UIBezierPath(rect: CGRectMake(
-                rect.width * CGFloat(i * 2 + 1) / CGFloat(data.count * 2) - barWidth / 2,
-                rect.height * CGFloat(1 - data[i] / globalMaxValueInGraph),
-                barWidth,
-                rect.height * CGFloat(data[i] / globalMaxValueInGraph)
-            ))
-            rectangle.fill()
-            UIColor.blackColor().setFill()
-            rectangle.stroke()
+        override func drawRect(rect: CGRect) {
+            for i in 0..<data.count {
+                let rectangle = UIBezierPath(rect: CGRectMake(
+                    rect.width * CGFloat(i * 2 + 1) / CGFloat(data.count * 2) - barWidth / 2,
+                    rect.height * CGFloat(1 - data[i] / globalMaxValueInGraph),
+                    barWidth,
+                    rect.height * CGFloat(data[i] / globalMaxValueInGraph)
+                    ))
+                rectangle.fill()
+                UIColor.blackColor().setFill()
+                rectangle.stroke()
+            }
         }
     }
 }
